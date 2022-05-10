604d23c55c5690b870f7f01868cec2ec8e84d929
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-05-10 19:07:19 +0500

    snackbar init

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,30 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
+      if(_counter % 3 == 0){
+        ScaffoldMessenger.of(context)
+        ..clearSnackBars()..showSnackBar(
+        SnackBar(
+          content: Container(
+            padding: const EdgeInsets.all(16.0),
+            height: 90.0,
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+            ),
+            child: Column(
+              children: [
+                const Text('Counter', style: TextStyle(fontSize: 24.0, color: Colors.white), textAlign: TextAlign.center,),
+                Text('the number is $_counter'),
+              ],
+            )),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          ),
+      );
+    }
   }
 
   @override
