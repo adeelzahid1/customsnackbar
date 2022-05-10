550aac361b0e914608ca4e6d510129330a1f5376
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-05-10 21:49:04 +0500

    svg assets add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,20 +41,37 @@ class _MyHomePageState extends State<MyHomePage> {
         ScaffoldMessenger.of(context)
         ..clearSnackBars()..showSnackBar(
         SnackBar(
-          content: Container(
-            padding: const EdgeInsets.all(16.0),
-            height: 90.0,
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          content: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                height: 90.0,
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
 
-            ),
-            child: Column(
-              children: [
-                const Text('Counter', style: TextStyle(fontSize: 24.0, color: Colors.white), textAlign: TextAlign.center,),
-                Text('the number is $_counter'),
-              ],
-            )),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 48),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Counter', style: TextStyle(fontSize: 24.0, color: Colors.white), textAlign: TextAlign.center,),
+                          Text('the number after increment  is $_counter' , maxLines: 2, overflow: TextOverflow.ellipsis,),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+                SvgPicture.asset('assets/images/next.svg',
+                 height: 40.0,
+                  width: 40.0,
+                //  color: Colors.white,
+                 )
+            ],
+          ),
           behavior: SnackBarBehavior.floating,
           backgroundColor: Colors.transparent,
           elevation: 0.0,
